0163aa6f48e7305e475390dcc95892acd3b30972
Яна <90270069+yaaanix@users.noreply.github.com>
2022-06-02 19:32:28 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,26 +2,14 @@ import telebot
 from telebot import types
 import requests
 from collections import defaultdict
+from flask import Flask, request
 
-APP_URL = f"https://sintezzz.herokuapp.com/{TOKEN}"
+
+
+bot = 'bot'
+botik = telebot.TeleBot(bot)
 server = Flask(__name__)
-
-@server.route("/" + TOKEN, methods=["POST"]
-def get_message()
-    json_string = request.get_data().decode("utf-8")
-    update = telebot.types.Update.de_json(json_string)
-    bot.process_new_updates([update])
-    return "!", 200
-
-
-@server.route("/")
-def webhook():
-    bot.remove_webhook()
-    bot.set_webhook(url=APP_URL)
-    return "!", 200
-
-if __name__ == "__main__":
-    server.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000))
+HEROKU_APPNAME = 'sintezzz'
 
 
 memory = defaultdict(dict)
@@ -166,3 +154,4 @@ def bot_message(message):
 
 
 bot.polling(none_stop=True)
+
