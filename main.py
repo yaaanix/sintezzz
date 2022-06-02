c28dec2b0cde36024d7922fa5186005498b7738e
Яна <90270069+yaaanix@users.noreply.github.com>
2022-06-02 18:58:50 +0300

    Update main.py

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ def webhook():
     return "!", 200
 
 if __name__ == "__main__":
-    server.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    server.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000))
 
 
 memory = defaultdict(dict)
