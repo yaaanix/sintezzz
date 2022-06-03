b371513b6b1eff6b8cd386702695d878e239491c
Яна <90270069+yaaanix@users.noreply.github.com>
2022-06-04 00:51:08 +0300

    Update main.py

--- main.py
+++ main.py
@@ -63,9 +63,9 @@ def bot_message(message):
             bot.send_message(message.chat.id, 'Хочешь загрузить свою сказку или послушать из библиотеки?', reply_markup = markup)
         elif message.text == 'Свою':
             markup = types.ReplyKeyboardMarkup(resize_keyboard = True)
-            item5 = types.KeyboardButton('Нажми, когда отправишь текст сказки, а потом не забудь нажать на кнопку)')
+            item5 = types.KeyboardButton('Нажми, когда отправишь текст сказки')
             markup.add(item5)
-            msg = bot.send_message(message.chat.id, "Напиши мне текст сказки", reply_markup=markup)
+            msg = bot.send_message(message.chat.id, "Напиши мне текст сказки, а потом не забудь нажать на кнопку)", reply_markup=markup)
             def send(message):
                 abby = message.text
                 memory[message.chat.id]['Текст'] = abby
@@ -89,7 +89,7 @@ def bot_message(message):
             markup.add(item6, item7, item8, item9, item10, item11, item12, item13, item14, item15)
             bot.send_message(message.chat.id, 'Выбери сказку из списка', reply_markup = markup)
         # скорость
-        elif message.text in ['Нажми, когда отправишь текст сказки']:
+        elif message.text in ['Напиши мне текст сказки, а потом не забудь нажать на кнопку)']:
             markup = types.ReplyKeyboardMarkup(resize_keyboard = True)
             item16 = types.KeyboardButton('Маленькая скорость')
             item17 = types.KeyboardButton('Средняя скорость')
